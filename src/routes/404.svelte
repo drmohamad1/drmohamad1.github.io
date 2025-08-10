820d40f7d884f72edd1a7019ea4854a3e6fa9db6
DrMohamad <mohamad42912@gmail.com>
2025-08-10 20:53:06 +0330

    add 404.svelte

---
+++ src/routes/404.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
+<div style="text-align: center; padding: 4rem; font-family: sans-serif;">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <a href="{base}/" style="color: blue; text-decoration: underline;">Go back home</a>
+</div>
